4fb4a33cf39e5fdd251067d31cc1deec8c825b22
一ノ瀬由芽 <srv123123@me.com>
2018-12-14 13:24:16 +0800

    Initial Commit

---
+++ timeCapsule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  timeCapsule
+//
+//  Created by 一ノ瀬由芽 on 2018/12/14.
+//  Copyright © 2018年 Yume Ichinose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
