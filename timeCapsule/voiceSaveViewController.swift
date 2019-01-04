f1962601e4a827dfc9a2858eaff357f7836ed4ae
一ノ瀬由芽 <srv123123@me.com>
2019-01-04 14:36:06 +0800

    setCategoryのエラーが出ています

--- timeCapsule/voiceSaveViewController.swift
+++ timeCapsule/voiceSaveViewController.swift
@@ -7,9 +7,24 @@
 //
 
 import UIKit
+import AVFoundation
 
-class voiceSaveViewController: UIViewController {
-
+class voiceSaveViewController: UIViewController,AVAudioRecorderDelegate, AVAudioPlayerDelegate {
+    
+//    使う部品の宣言
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var playButton: UIButton!
+    
+//    使う変数の宣言
+//    falseはレコードオフの状態
+    var audioRecorder: AVAudioRecorder!
+    var audioPlayer: AVAudioPlayer!
+    var isRecording = false
+    var isPlaying = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,10 +39,94 @@ class voiceSaveViewController: UIViewController {
         shadowView.layer.shadowOffset = CGSize(width: 5, height: 5)
         shadowView.layer.shadowRadius = 5
 
-        // Do any additional setup after loading the view.
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
 
+    
+    @IBAction func record(){
+        if !isRecording {
+            
+            let session = AVAudioSession.sharedInstance()
+            try! session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:.allowBluetoothA2DP)
+            try! session.setActive(true)
+            
+            let settings = [
+                AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                AVSampleRateKey: 44100,
+                AVNumberOfChannelsKey: 2,
+                AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+            ]
+            
+            audioRecorder = try! AVAudioRecorder(url: getURL(), settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+            
+            isRecording = true
+            
+            label.text = "録音中"
+            recordButton.setBackgroundImage(UIImage(named: "microphone2.png"), for: .normal)
+            playButton.isEnabled = false
+            
+        }else{
+            
+            audioRecorder.stop()
+            isRecording = false
+            
+            label.text = "待機中"
+            recordButton.setBackgroundImage(UIImage(named: "microphone.png"), for: .normal)
+            playButton.isEnabled = true
+            
+        }
+    }
+    
+    @IBAction func play(){
+        if !isPlaying {
+            
+            audioPlayer = try! AVAudioPlayer(contentsOf: getURL())
+            audioPlayer.delegate = self
+            audioPlayer.play()
+            
+            isPlaying = true
+            
+            label.text = "再生中"
+            playButton.setBackgroundImage(UIImage(named: "stopBtn.png"),for: .normal)
+            recordButton.isEnabled = false
+            
+        }else{
+            
+            audioPlayer.stop()
+            isPlaying = false
+            
+            label.text = "待機中"
+            playButton.setBackgroundImage(UIImage(named: "playBtn.png"), for: .normal)
+            recordButton.isEnabled = true
+            
+        }
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        if flag {
+            
+            audioPlayer.stop()
+            isPlaying = false
+            
+            label.text = "待機中"
+            playButton.setBackgroundImage(UIImage(named: "playBtn.png"), for: .normal)
+            recordButton.isEnabled = true
+            
+        }
+    }
+    
+    func getURL() -> URL{
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let docsDirect = paths[0]
+        let url = docsDirect.appendingPathComponent("recording.m4a")
+        return url
+    }
     /*
     // MARK: - Navigation
 
